9c0c460f76d94def24d18843fb02ec834d53d961
Wyatt Fong <fongwyatt@gmail.com>
2025-05-09 19:02:56 -0700

    Finished expose section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+    return JSON.parse(localStorage.getItem("recipes"));
 }
 
 /**
@@ -34,6 +35,12 @@ function getRecipesFromStorage() {
  * @param {Array<Object>} recipes An array of recipes
  */
 function addRecipesToDocument(recipes) {
+    const main = document.querySelector("main");
+    for (let i = 0; i < recipes.length; i++) {
+        const recipe = document.createElement("recipe-card");
+        recipe.data = recipes[i];
+        main.append(recipe);
+    }
 	// A10. TODO - Get a reference to the <main> element
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
